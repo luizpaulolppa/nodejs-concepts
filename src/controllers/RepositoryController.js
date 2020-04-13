5236245eb12563b49036f3323fcea661b7879795
Luiz Paulo Pilegi de Almeida <luizpilegilppa@gmail.com>
2020-04-12 22:03:19 -0300

    create new routes

--- src/controllers/RepositoryController.js
+++ src/controllers/RepositoryController.js
@@ -1,21 +1,136 @@
 const express = require("express");
+const uuid = require("uuid/v4");
 const router = express.Router();
-const repositories = [];
+let repositories = [];
 
 router.get("/", (req, res) => {
-  return res.json(repositories);
+  const { name } = req.query;
+
+  const repos = name
+    ? repositories.filter(repo => 
+      repo.name.toUpperCase().includes(name.toUpperCase()))
+    : repositories;
+  
+  return res.json(repos);
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const repository = repositories.find(repo => repo.id === id);
+  if (!repository) {
+    return res.status(400).json({ message: "Not found repository." });
+  }
+
+  return res.json(repository);
 });
 
 router.post("/", (req, res) => {
-  return res.json({ message: "OK" });
+  const { name, description, link } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ message: "Not found name." });
+  }
+
+  if (!description) {
+    return res.status(400).json({ message: "Not found description." });
+  }
+
+  if (!link) {
+    return res.status(400).json({ message: "Not found link." });
+  }
+
+  const matchReposName = repositories.find(repo => repo.name == name);
+  if (matchReposName) {
+    return res.status(400).json({ message: "fond the same name." });
+  }
+  
+  const repository = { id: uuid(), name, description, link, likes: 0, dislikes: 0 };
+
+  repositories.push(repository);
+
+  return res.json(repository);
 });
 
-router.put("/", (req, res) => {
-  return res.json({ message: "OK" });
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, description, link } = req.body;
+
+  const repository = repositories.find(repo => repo.id === id);
+  if (!repository) {
+    return res.status(400).json({ message: "Not found repository." });
+  }
+
+  if (!name) {
+    return res.status(400).json({ message: "Not found name." });
+  }
+
+  if (!description) {
+    return res.status(400).json({ message: "Not found description." });
+  }
+
+  if (!link) {
+    return res.status(400).json({ message: "Not found link." });
+  }
+
+  const matchReposName = repositories
+    .filter(repo => repo.id !== id)
+    .find(repo => repo.name == name);
+
+  if (matchReposName) {
+    return res.status(400).json({ message: "fond the same name." });
+  }
+
+  repository.name = name;
+  repository.description = description;
+  repository.link = link;
+
+  repositories = repositories.map(repo => repo.id === id ? repository : repo);
+
+  return res.json(repository);
 });
 
-router.delete("/", (req, res) => {
-  return res.json({ message: "OK" });
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const repository = repositories.find(repo => repo.id === id);
+  if (!repository) {
+    return res.status(400).json({ message: "Not found repository." });
+  }
+
+  repositories = repositories.filter(repo => repo.id !== id);
+
+  return res.status(204).send();
+});
+
+router.post("/:id/likes", (req, res) => {
+  const { id } = req.params;
+
+  const repository = repositories.find(repo => repo.id === id);
+  if (!repository) {
+    return res.status(400).json({ message: "Not found repository." });
+  }
+
+  repository.likes = repository.likes +1;
+
+  repositories = repositories.map(repo => repo.id === id ? repository : repo);
+
+  return res.json(repository);
+});
+
+router.post("/:id/dislikes", (req, res) => {
+  const { id } = req.params;
+
+  const repository = repositories.find(repo => repo.id === id);
+  if (!repository) {
+    return res.status(400).json({ message: "Not found repository." });
+  }
+
+  repository.dislikes = repository.dislikes +1;
+
+  repositories = repositories.map(repo => repo.id === id ? repository : repo);
+
+  return res.json(repository);
 });
 
 module.exports = router;
